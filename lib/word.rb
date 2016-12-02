607442af83598f06b8aeb5a5180be7d1bf9c33d7
Joshua Rinard <jrinard@hotmail.com>
2016-12-02 09:56:28 -0800

    Unit specs for all, save, clear, find plus unit logic passing

--- lib/word.rb
+++ lib/word.rb
@@ -4,6 +4,7 @@ class Word
   define_method(:initialize) do |word|
     @word = word
     @id = @@word_array.length().+(1)
+    @definition_array = []
   end
 
   define_method(:word) do
@@ -13,6 +14,33 @@ class Word
   define_method(:id) do
     @id
   end
+
+  define_method(:definition) do
+    @definition_array
+  end
+
+  define_singleton_method(:all) do
+    @@word_array
+  end
+
+  define_method(:save) do
+    @@word_array.push(self)
+  end
+
+  define_singleton_method(:clear) do
+    @@word_array = []
+  end
+
+  define_singleton_method(:find) do |id|
+    found_word = nil
+    @@word_array.each() do |temp_word|
+      if temp_word.id().eql?(id)
+        found_word = temp_word
+      end
+    end
+    found_word
+  end
+
 
 
 
--- spec/word_spec.rb
+++ spec/word_spec.rb
@@ -3,6 +3,9 @@ require('word')
 # require('definition')
 
 describe(Word) do
+  before() do
+    Word.clear()
+  end
 
   describe('#word') do
     it('returns the word') do
@@ -18,7 +21,44 @@ describe(Word) do
     end
   end
 
+  describe('#definition') do
+    it("returns the definition of the word") do
+      test_word = Word.new('Water')
+      expect(test_word.definition()).to(eq([]))
+    end
+  end
 
+  describe('#save') do
+    it('adds a word to the array of saved words') do
+      test_word = Word.new('water')
+      test_word.save()
+      expect(Word.all()).to(eq([test_word]))
+    end
+  end
+
+  describe(".all") do
+    it("is empty at first") do
+      expect(Word.all()).to(eq([]))
+    end
+  end
+
+  describe(".clear") do
+    it("empties out all of the saved words") do
+      Word.new("water").save()
+      Word.clear()
+      expect(Word.all()).to(eq([]))
+    end
+  end
+
+  describe('find') do
+    it('returns a word by its id number') do
+      test_word = Word.new('Water')
+      test_word.save()
+      test_word2 = Word.new('Fire')
+      test_word2.save()
+      expect(Word.find(test_word.id())).to(eq(test_word))
+    end
+  end
 
 
 
