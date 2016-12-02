9633e68c292eb6f427b5e5df95007b8ea8a2a7c9
Joshua Rinard <jrinard@hotmail.com>
2016-12-02 11:15:25 -0800

    add integration spec with 3 passing specs

--- spec/word_integration_spec.rb
+++ spec/word_integration_spec.rb
@@ -10,3 +10,20 @@ describe('the word list file path', {:type => :feature}) do
     expect(page).to have_content('Word List')
   end
 end
+
+describe('the add word form file path', {:type => :feature}) do
+  it('processes the user click to return add word form page') do
+    visit('/')
+    click_link('Add New Word')
+    expect(page).to have_content('Add word')
+  end
+end
+
+describe('the add new word success file path', {:type => :feature}) do
+  it('processes the input and click to return success page') do
+    visit('/word/new')
+    fill_in('word', :with => 'Rock')
+    click_button('Save')
+    expect(page).to have_content('Success')
+  end
+end
